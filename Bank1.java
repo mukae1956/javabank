431e668f2a54fbf41191db923e5f416a173082c0
소정 <simpson9412@gmail.com>
2026-03-28 17:13:52 +0900

    국민은행 적금 파일 및 입출력

---
+++ Bank1.java
@@ -0,0 +1,64 @@
+package Project;
+
+import java.io.*;
+import java.util.*;
+
+public class Bank1 {
+
+    // 👉 클래스는 여기 있어야 함 (main 밖!)
+    static class Product {
+        String name;
+        int period;
+        double baseRate;
+        double maxRate;
+
+        public Product(String name, int period, double baseRate, double maxRate) {
+            this.name = name;
+            this.period = period;
+            this.baseRate = baseRate;
+            this.maxRate = maxRate;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        List<Product> list = readCsv("KBbank.csv");
+
+        System.out.println("=== 전체 상품 ===");
+
+        for (Product p : list) {
+            System.out.println(p.name + " | " + p.period + "개월 | " + p.maxRate + "%");
+        }
+    }
+    public static List<Product> readCsv(String filePath) {
+        List<Product> list = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(filePath), "CP949")
+            );
+
+            String line;
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String name = data[0];
+                int period = Integer.parseInt(data[1]);
+                double baseRate = Double.parseDouble(data[2]);
+                double maxRate = Double.parseDouble(data[3]);
+
+                Product p = new Product(name, period, baseRate, maxRate);
+                list.add(p);
+            }
+
+            br.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+}
